48d99b68fdbdc267c01f90c17ea73f9c20e38cd5
Shamim Hossain <hossains159@gmail.com>
2021-07-23 09:30:29 +0600

    refactor: replace defineComponents by setup tag in HelloWorld component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,70 +1,56 @@
 <template>
-  <h1>{{ msg }}</h1>
+    <h1>{{ msg }}</h1>
 
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a
-      href="https://marketplace.visualstudio.com/items?itemName=octref.vetur"
-      target="_blank"
-    >
-      Vetur
-    </a>
-    or
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    (if using
-    <code>&lt;script setup&gt;</code>)
-  </p>
+    <p>
+        Recommended IDE setup:
+        <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+        +
+        <a
+            href="https://marketplace.visualstudio.com/items?itemName=octref.vetur"
+            target="_blank"
+        >
+            Vetur
+        </a>
+        or
+        <a href="https://github.com/johnsoncodehk/volar" target="_blank"
+            >Volar</a
+        >
+        (if using
+        <code>&lt;script setup&gt;</code>)
+    </p>
 
-  <p>See <code>README.md</code> for more information.</p>
+    <p>See <code>README.md</code> for more information.</p>
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
+    <p>
+        <a href="https://vitejs.dev/guide/features.html" target="_blank">
+            Vite Docs
+        </a>
+        |
+        <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+    </p>
 
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+    <button type="button" @click="count++">count is: {{ count }}</button>
+    <p>
+        Edit
+        <code>components/HelloWorld.vue</code> to test hot module replacement.
+    </p>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent } from 'vue'
-export default defineComponent({
-  name: 'HelloWorld',
-  props: {
-    msg: {
-      type: String,
-      required: true
-    }
-  },
-  setup: () => {
-    const count = ref(0)
-    return { count }
-  }
-})
+export default {
+    name: 'HelloWorld',
+}
 </script>
 
-<style scoped>
-a {
-  color: #42b983;
-}
+<script lang="ts" setup>
+import { ref, defineProps } from 'vue'
 
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
+const props = defineProps({
+    msg: {
+        type: String,
+        required: true,
+    },
+})
 
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
-</style>
+const count = ref(0)
+</script>
