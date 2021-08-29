90fd6b58e5ac78e44d1f87a9755d93ce58130324
Shamim Hossain <hossains159@gmail.com>
2021-08-29 22:15:11 +0600

    feat: add 404 custom layout and page

---
+++ src/layouts/404.vue
@@ -0,0 +1,19 @@
+<template>
+    <main class="max-w-5xl p-5 mx-auto my-10 text-center">
+        <router-view />
+        <div>
+            <v-button @click="router.back()"> Go Back </v-button>
+        </div>
+    </main>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+import { useMeta } from 'vue-meta'
+
+useMeta({
+    title: '404 - Not found!',
+})
+
+const router = useRouter()
+</script>
---
+++ src/pages/[...all].vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="py-4 text-2xl text-center">Page Not found!</div>
+</template>
+
+<route lang="yaml">
+meta:
+    layout: 404
+</route>
