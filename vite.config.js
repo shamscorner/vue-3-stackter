261aa7930591135e257f1a2d4d470f2d1a13c31c
Shamim Hossain <hossains159@gmail.com>
2021-08-29 21:32:26 +0600

    feat: add ~/ alias to /src in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    resolve: {
+        alias: {
+            '~/': `${path.resolve(__dirname, 'src')}/`,
+        },
+    },
     plugins: [vue(), Pages()],
 })
